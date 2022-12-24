c285528e6ea28ddfebb534d12f6a5913174562fd
Steelswarm <antoniomiguelbraga@live.com.pt>
2022-12-24 21:47:36 +0100

    Added Oculus SDK, Scipt Inspector 3, Fullscreen Editor and VR Hands packages

--- Integration of Arduino+Raspberrypi+Unity/Arduino/circuit arduino code/circuit arduino code.ino
+++ Integration of Arduino+Raspberrypi+Unity/Arduino/circuit arduino code/circuit arduino code.ino
@@ -192,17 +192,18 @@ void loop()
   Serial.print("Force sensor reading = ");
  // Serial.print(analogReading); // print the raw analog reading
 
-  if (analogReading < 10)       // from 0 to 9
-    Serial.println(" -> no pressure");
-  else if (analogReading < 200) // from 10 to 199
-    Serial.println(" -> light touch");
-  else if (analogReading < 500) // from 200 to 499
-    Serial.println(" -> light squeeze");
-  else if (analogReading < 800) // from 500 to 799
-    Serial.println(" -> medium squeeze");
-  else // from 800 to 1023
-    Serial.println(" -> big squeeze");
+  // if (analogReading < 10)       // from 0 to 9
+  //   Serial.println(" -> no pressure");
+  // else if (analogReading < 200) // from 10 to 199
+  //   Serial.println(" -> light touch");
+  // else if (analogReading < 500) // from 200 to 499
+  //   Serial.println(" -> light squeeze");
+  // else if (analogReading < 800) // from 500 to 799
+  //   Serial.println(" -> medium squeeze");
+  // else // from 800 to 1023
+  //   Serial.println(" -> big squeeze");
     Serial.println(analogReading);
+    Serial.println("----------------------");
   delay(1000);
 
 }
